87d136437a5f3ee6656a54533e761f0a3de52295
つっちー <matsu.tis2022@gmail.com>
2022-06-07 11:33:44 +0900

    商品一覧表示、商品詳細ページ作成 課題：商品一覧のパスが「findAll」になっている。

--- src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
+++ src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
@@ -3,6 +3,7 @@ package jp.co.sss.shop.controller.item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sss.shop.repository.ItemRepository;
@@ -29,9 +30,22 @@ public class ItemShowCustomerController {
 	 */
 	@RequestMapping(path = "/")
 	public String index(Model model) {
-
+		model.addAttribute("items", itemRepository.findAll());
 		
 		return "index";
+	}
+	
+	@RequestMapping(path = "/item/list/findAll")
+	public String itemListFindAll(Model model) {
+		model.addAttribute("items", itemRepository.findAll());
+		return "item/list/item_list";
+	}
+	
+	@RequestMapping(path = "/item/detail/{id}")
+	public String itemDetail(@PathVariable int id, Model model) {
+		model.addAttribute("item", itemRepository.getById(id));
+		
+		return "item/detail/item_detail";
 	}
 
 	
