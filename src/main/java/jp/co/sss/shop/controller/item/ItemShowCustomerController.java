72da363779ef9e79d750ad7a9a9748f910f28a21
ChisatoOmori <omori.tis2022@gmail.com>
2022-06-20 17:32:15 +0900

    治っていないhtmlなどを修正

--- src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
+++ src/main/java/jp/co/sss/shop/controller/item/ItemShowCustomerController.java
@@ -80,7 +80,6 @@ public class ItemShowCustomerController {
 				emptyFav.setIsFav(0);
 				emptyFav.setItemId(item.getId());
 				emptyList.add(emptyFav);
-				System.out.println("itmeid:" + emptyFav.getItemId());
 			}
 			List<Favorite> favorites = favoriteRepository.findByUserIdOrderByItemId(userId);
 			if(emptyList.size() > favorites.size()) {
@@ -90,19 +89,16 @@ public class ItemShowCustomerController {
 						try {
 							if(fav2.getItemId() == fav.getItemId()) {
 								emptyList.set(cnt, fav2);
-								System.out.println(cnt + "回目if id:" + fav2.getItemId());
 							}else {
 								Favorite emptyFav = new Favorite();
 								emptyFav.setItemId(fav.getItemId());
 								emptyFav.setIsFav(fav.getIsFav());
 								emptyList.set(cnt, emptyFav);
-								System.out.println(cnt + "回目catch id:" + fav.getItemId());
 							}
 						}catch(IndexOutOfBoundsException e){
 							
 						}
 					}
-
 					cnt++;
 				}
 				model.addAttribute("favorites", emptyList);
@@ -150,7 +146,7 @@ public class ItemShowCustomerController {
 		//ログインしているかどうか確認
 		if(session.getAttribute("user") != null) {
 			Integer userId = ((UserBean) session.getAttribute("user")).getId();
-			List<Item> items = itemRepository.findByDeleteFlag(0);
+			List<Item> items = itemRepository.findByDeleteFlagOrderByInsertDateDescIdAsc(0);
 			List<Favorite> emptyList = new ArrayList<Favorite>();
 			for(Item item : items) {
 				Favorite emptyFav = new Favorite();
@@ -162,16 +158,19 @@ public class ItemShowCustomerController {
 			if(emptyList.size() > favorites.size()) {
 				int cnt = 0;
 				for(Favorite fav : emptyList) {
-					try {
-						if(favorites.get(cnt).getItemId() == fav.getItemId()) {
-							emptyList.set(cnt, favorites.get(cnt));
-						}else {
-							Favorite emptyFav = new Favorite();
-							emptyFav.setIsFav(0);
-							emptyList.set(cnt, emptyFav);
+					for(Favorite fav2 : favorites) {
+						try {
+							if(fav2.getItemId() == fav.getItemId()) {
+								emptyList.set(cnt, fav2);
+							}else {
+								Favorite emptyFav = new Favorite();
+								emptyFav.setItemId(fav.getItemId());
+								emptyFav.setIsFav(fav.getIsFav());
+								emptyList.set(cnt, emptyFav);
+							}
+						}catch(IndexOutOfBoundsException e){
+							
 						}
-					}catch(IndexOutOfBoundsException e){
-						break;
 					}
 					cnt++;
 				}
