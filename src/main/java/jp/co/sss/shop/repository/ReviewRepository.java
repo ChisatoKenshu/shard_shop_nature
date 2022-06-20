d7c53176f349e005dff88698b4d27a573edf9cd3
Yokota <shinji.yokota.tis2022@gmail.com>
2022-06-20 17:39:37 +0900

    create method and view from ReviewShowAdminController

--- src/main/java/jp/co/sss/shop/repository/ReviewRepository.java
+++ src/main/java/jp/co/sss/shop/repository/ReviewRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import jp.co.sss.shop.entity.Review;
 
@@ -12,10 +13,11 @@ import jp.co.sss.shop.entity.Review;
  *
  * @author 横田
  */
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 	
 	// 特定商品のレビュー全てを日時の新しい順に取得
-	@Query("SELECT r.id FROM Review r WHERE r.item.id = :itemId ORDER BY r.insertDate DESC, r.id ASC")
+	@Query("SELECT r FROM Review r WHERE r.item.id = :itemId ORDER BY r.insertDate DESC")
 	public List<Review> findByReviewWhereItemOrderByInsertDateDescQuery(Integer itemId);
 
 }
