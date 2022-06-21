a24c071caa4646ba5d75295f8d3dc8a02cb2f78e
Yokota <shinji.yokota.tis2022@gmail.com>
2022-06-21 11:38:43 +0900

    delete finction complete!

--- src/main/java/jp/co/sss/shop/controller/review/ReviewDeleteCustomerController.java
+++ src/main/java/jp/co/sss/shop/controller/review/ReviewDeleteCustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.sss.shop.entity.Review;
 import jp.co.sss.shop.repository.ReviewRepository;
@@ -24,22 +25,59 @@ public class ReviewDeleteCustomerController {
 	ReviewRepository reviewRepository;
 	
 	/**
-	 * 商品情報削除確認処理
+	 * レビュー情報削除確認処理
 	 *
-	 * @param id 商品ID
+	 * @param id レビューID
 	 * @param model Viewとの値受渡し
-	 * @return "item/delete/item_delete_check" 商品情報 削除確認画面へ
+	 * @return "review/delete/review_delete_check" レビュー情報 削除確認画面へ
 	 */
 	@RequestMapping(path = "/review/delete/check", method = RequestMethod.POST)
 	public String deleteCheck(Integer id, Model model) {
 	
-		// 削除対象の商品情報を取得
+		// 削除対象のレビュー情報を取得
 		Review review = reviewRepository.getById(id);
 
-		// 商品情報をViewに渡す
+		// レビュー情報をViewに渡す
 		model.addAttribute("review", review);
 
 		return "review/delete/review_delete_check";
+	}
+	
+	/**
+	 * レビュー情報削除完了処理
+	 *
+	 * @param id レビューID
+	 * @return "redirect:/review/delete/complete" レビュー情報 削除完了画面へ
+	 */
+	@RequestMapping(path = "/review/delete/complete", method = RequestMethod.POST)
+	public String deleteComplete(Integer id, RedirectAttributes redirectAttributes) {
+		
+		// 削除対象のレビュー情報から商品IDを取得しリダイレクト先に受け渡す
+		Review review = reviewRepository.getById(id);
+		Integer itemId = review.getItem().getId();
+		redirectAttributes.addFlashAttribute("itemId", itemId);
+
+		// 削除対象のレビュー情報を物理削除
+		reviewRepository.deleteById(id);
+
+		return "redirect:/review/delete/complete";
+	}
+	
+	/**
+	 * レビュー情報削除完了画面表示
+	 * 
+	 * @return "review/delete/review_delete_complete"  レビュー情報 削除完了画面へ
+	 */
+	@RequestMapping(path = "/review/delete/complete", method = RequestMethod.GET)
+	public String deleteCompleteRedirect(Model model) {
+		
+		// リダイレクト前に受け渡した情報を取得
+		Integer itemId = (Integer) model.getAttribute("itemId");
+		
+		// レビュー情報をViewに渡す
+		model.addAttribute("itemId", itemId);
+
+		return "review/delete/review_delete_complete";
 	}
 
 }
