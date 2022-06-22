e2f2364e34d2b94fb32391c62d26e661f462fc4c
つっちー <matsu.tis2022@gmail.com>
2022-06-22 11:11:40 +0900

    評価値の平均を算出

--- src/main/java/jp/co/sss/shop/repository/ReviewRepository.java
+++ src/main/java/jp/co/sss/shop/repository/ReviewRepository.java
@@ -24,4 +24,8 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 	@Query("SELECT r FROM Review r WHERE r.item.id = :itemId AND r.permissionFlag = 1 ORDER BY r.insertDate DESC")
 	public List<Review> findByReviewWhereItemANDPermissionFlagOrderByInsertDateDescQuery(Integer itemId);
 
+	// 特定商品のレビュー平均を算出
+	@Query("SELECT ROUND(AVG(r.evaluation), 1) FROM Review r WHERE r.permissionFlag = 1 AND r.item.id = :itemId GROUP BY r.item.id")
+	public Double findAVGByPermissionFlagGroupByItemIdQuery(Integer itemId);
+
 }
